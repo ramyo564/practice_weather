c61cf50b7b4b2f2b9f397c1c3cd8d1675e940703
ramyo564 <yohan032yohan@gmail.com>
2024-02-01 21:22:56 +0900

    Json 파싱하기

--- weather/weather/src/main/java/zerobase/weather/service/DiaryService.java
+++ weather/weather/src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,5 +1,8 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -15,10 +20,11 @@ public class DiaryService {
     private String apiKey;
     public void createDiary(LocalDate date, String text){
         // open weather map 에서 날씨 가져오기
-        String weatherDate = getWeatherString();
+        String weatherData = getWeatherString();
 
         // 받아온 날씨 json 파싱하기
-
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
+        
         // 파싱된 데이터 + 일기 값 db에 넣기
     }
     private String getWeatherString(){
@@ -47,6 +53,23 @@ public class DiaryService {
         } catch (Exception e){
             return "failed to get response";
         }
+    }
 
+    private Map<String, Object> parseWeather(String jsonString){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try{
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        }catch (ParseException e){
+            throw new RuntimeException(e);
+        }
+        Map<String, Object> resultMap = new HashMap<>();
+        JSONObject mainData = (JSONObject) jsonObject.get("weather");
+        resultMap.put("temp", mainData.get("temp"));
+        JSONObject weatherData = (JSONObject) jsonObject.get("main");
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+        return resultMap;
     }
 }
